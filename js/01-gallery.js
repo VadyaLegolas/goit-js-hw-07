edba936d78782c201a9d2eddcd30aa182f538549
Vlad <vadya-85@mail.ru>
2023-08-07 20:41:21 +0200

    add task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const refs = {
+  galleryUl: document.querySelector(".gallery"),
+};
+// console.log(galleryItems);
+const galleryMarkup = createGalleryMarkup(galleryItems);
+refs.galleryUl.insertAdjacentHTML("beforeend", galleryMarkup);
 
-console.log(galleryItems);
+refs.galleryUl.addEventListener("click", onGalleryUlClick);
+
+function createGalleryMarkup(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+            <a class="gallery__link" href="large-image.jpg">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+        </li>`;
+    })
+    .join("");
+}
+
+function onGalleryUlClick(e) {
+  e.preventDefault();
+  const img = e.target;
+  if (!img.classList.contains('gallery__image')) {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <img src="${img.dataset.source}" width="800" height="600">
+  `);
+
+  instance.show();
+
+  document.addEventListener("keydown", (event) => {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  });
+}
