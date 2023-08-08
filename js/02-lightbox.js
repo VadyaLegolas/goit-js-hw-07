63bba74353c64f4a663419811f8a05587c8a87e4
Vlad <vadya-85@mail.ru>
2023-08-08 23:39:50 +0200

    upd task1 and task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -10,13 +10,31 @@ refs.galleryUl.insertAdjacentHTML("beforeend", galleryMarkup);
 
 refs.galleryUl.addEventListener("click", onGalleryUlClick);
 
+if ("loading" in HTMLImageElement.prototype) {
+  // supported in browser
+  const images = document.querySelectorAll('img[loading="lazy"]');
+  images.forEach((img) => {
+    img.src = img.dataset.src;
+  });
+} else {
+  // fetch polyfill/third-party library
+  const script = document.createElement("script");
+  script.src =
+    "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js";
+  script.integrity =
+    "sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ==";
+  script.crossOrigin = "anonymous";
+  script.referrerPolicy = "no-referrer";
+  document.body.appendChild(script);
+}
+
 function createGalleryMarkup(gallery) {
   return gallery
     .map(({ preview, original, description }) => {
       return `
         <li class="gallery__item">
             <a class="gallery__link" href="${original}">
-                <img class="gallery__image" src="${preview}" alt="${description}" />
+                <img loading="lazy" class="gallery__image" data-src="${preview}" alt="${description}" />
             </a>
         </li>`;
     })
