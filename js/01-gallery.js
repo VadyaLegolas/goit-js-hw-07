af04aaefd0ad89dd4421e9456f7c65a8347aeada
Vlad <vadya-85@mail.ru>
2023-08-08 23:46:15 +0200

    fix lazyload

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -18,6 +18,10 @@ if ("loading" in HTMLImageElement.prototype) {
   });
 } else {
   // fetch polyfill/third-party library
+  const images = document.querySelectorAll('img[loading="lazy"]');
+  images.forEach((img) => {
+    img.classList.add("lazyload");
+  });
   const script = document.createElement("script");
   script.src =
     "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js";
